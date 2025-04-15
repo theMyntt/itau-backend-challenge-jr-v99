e65706feffa303b6e1d10b7d46e2f9885ea4a844
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-14 22:01:15 -0300

    feat: initial commit

---
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/V99ItauChallengeJrApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielaraujo.v99_itau_challenge_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V99ItauChallengeJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V99ItauChallengeJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielaraujo/v99_itau_challenge_jr/V99ItauChallengeJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielaraujo.v99_itau_challenge_jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V99ItauChallengeJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
