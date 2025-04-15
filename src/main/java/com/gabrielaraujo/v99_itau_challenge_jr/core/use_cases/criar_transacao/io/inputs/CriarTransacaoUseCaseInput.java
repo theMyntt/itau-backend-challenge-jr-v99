3994f0f7fbb1b546fc92e12aa32894e46a65cb64
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-15 00:15:12 -0300

    chore: rules for validation on input

--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/core/use_cases/criar_transacao/io/inputs/CriarTransacaoUseCaseInput.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/core/use_cases/criar_transacao/io/inputs/CriarTransacaoUseCaseInput.java
@@ -1,11 +1,15 @@
 package com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.criar_transacao.io.inputs;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
 
 @Data
 public class CriarTransacaoUseCaseInput {
+    @NotNull
     private Double valor;
+
+    @NotNull
     private OffsetDateTime dataHora;
 }
