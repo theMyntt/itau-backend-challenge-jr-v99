71727d6d71bc3b8d024631b5c500c4655c58d1d5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-15 20:39:23 -0300

    docs: tag for statistics controller

--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/estatisticas_recentes/EstatisticasRecentesController.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/estatisticas_recentes/EstatisticasRecentesController.java
@@ -1,6 +1,7 @@
 package com.gabrielaraujo.v99_itau_challenge_jr.controllers.estatisticas_recentes;
 
 import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.estatisticas_recentes.EstatisticasRecentesUseCase;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.DoubleSummaryStatistics;
 
 @RestController
+@Tag(name = "Estatísticas", description = "Estatísticas da Aplicação e das Transações")
 public class EstatisticasRecentesController {
     private final EstatisticasRecentesUseCase estatisticasRecentesUseCase;
 
