e0d16e39534d6e3a96e96995ef7ae06129325aa5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-15 20:24:55 -0300

    chore: logging when errors

--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/ControllerAdvice.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/ControllerAdvice.java
@@ -2,19 +2,29 @@ package com.gabrielaraujo.v99_itau_challenge_jr.controllers;
 
 import com.gabrielaraujo.v99_itau_challenge_jr.core.entities.exceptions.NenhumaTransacaoEncontradaException;
 import com.gabrielaraujo.v99_itau_challenge_jr.core.entities.exceptions.CriteriosNaoCorrespondidosException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
     @ExceptionHandler(CriteriosNaoCorrespondidosException.class)
-    public ResponseEntity<?> manipularExcecao(CriteriosNaoCorrespondidosException excecao) {
+    public ResponseEntity<?> manipularCriteriosNaoCorrespondidosExcecao(CriteriosNaoCorrespondidosException excecao) {
+        log.error(excecao.getMessage());
         return ResponseEntity.status(422).build();
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> manipularExcecoes(RuntimeException exception) {
+        log.error(exception.getMessage());
+        return ResponseEntity.status(500).build();
+    }
+
     @ExceptionHandler(NenhumaTransacaoEncontradaException.class)
-    public ResponseEntity<?> manipularNotFoundExcecao(NenhumaTransacaoEncontradaException excecao) {
+    public ResponseEntity<?> manipularNenhumaTransacaoEcontradaExcecao(NenhumaTransacaoEncontradaException excecao) {
+        log.error(excecao.getMessage());
         return ResponseEntity.status(404).build();
     }
 }
