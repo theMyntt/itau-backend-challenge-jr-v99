edc482810ee0b23b3f3e4a6bb9322de84db1d8ee
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-15 00:07:52 -0300

    fix: missing http method annotation

--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/deletar_transacoes/DeletarTransacoesController.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/deletar_transacoes/DeletarTransacoesController.java
@@ -3,6 +3,7 @@ package com.gabrielaraujo.v99_itau_challenge_jr.controllers.deletar_transacoes;
 import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.deletar_transacoes.DeletarTransacoesUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @Controller
 public class DeletarTransacoesController {
@@ -12,6 +13,7 @@ public class DeletarTransacoesController {
         this.deletarTransacoesUseCase = deletarTransacoesUseCase;
     }
 
+    @DeleteMapping("/transacao")
     public ResponseEntity<?> performar() {
         deletarTransacoesUseCase.executar();
         return ResponseEntity.status(200).build();
--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/estatisticas_recentes/EstatisticasRecentesController.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/estatisticas_recentes/EstatisticasRecentesController.java
@@ -3,6 +3,7 @@ package com.gabrielaraujo.v99_itau_challenge_jr.controllers.estatisticas_recente
 import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.estatisticas_recentes.EstatisticasRecentesUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.DoubleSummaryStatistics;
 
@@ -14,6 +15,7 @@ public class EstatisticasRecentesController {
         this.estatisticasRecentesUseCase = estatisticasRecentesUseCase;
     }
 
+    @GetMapping("/estatistica")
     public ResponseEntity<DoubleSummaryStatistics> performar() {
         var resultado = estatisticasRecentesUseCase.executar();
         return ResponseEntity.status(200).body(resultado.getEstatisticas());
