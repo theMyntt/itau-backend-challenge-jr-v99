c413b93eeaa1086f0ef3213c5c26c2b6fe793173
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-15 00:11:38 -0300

    refactor: CriteriosNaoCorrespondidosException name

--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/core/entities/implementations/TransacaoEntityImpl.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/core/entities/implementations/TransacaoEntityImpl.java
@@ -1,7 +1,7 @@
 package com.gabrielaraujo.v99_itau_challenge_jr.core.entities.implementations;
 
 import com.gabrielaraujo.v99_itau_challenge_jr.core.entities.TransacaoEntity;
-import com.gabrielaraujo.v99_itau_challenge_jr.core.entities.exceptions.OneOrMoreCriteriaWereNotMetException;
+import com.gabrielaraujo.v99_itau_challenge_jr.core.entities.exceptions.CriteriosNaoCorrespondidosException;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -18,7 +18,7 @@ public class TransacaoEntityImpl extends TransacaoEntity {
     @Override
     public void comValor(double valor) {
         if (valor < 0) {
-            throw new OneOrMoreCriteriaWereNotMetException("valor", valor);
+            throw new CriteriosNaoCorrespondidosException("valor", valor);
         }
 
         this.valor = valor;
@@ -27,7 +27,7 @@ public class TransacaoEntityImpl extends TransacaoEntity {
     @Override
     public void comHora(OffsetDateTime dataHora) {
         if (dataHora.isAfter(OffsetDateTime.now())) {
-            throw new OneOrMoreCriteriaWereNotMetException("dataHora", dataHora.toString());
+            throw new CriteriosNaoCorrespondidosException("dataHora", dataHora.toString());
         }
 
         this.dataHora = dataHora;
