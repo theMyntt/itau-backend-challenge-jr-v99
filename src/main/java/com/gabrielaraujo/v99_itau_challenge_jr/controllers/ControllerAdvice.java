e10baba81ddb29a36d48953f7bb7f14bd36db76e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-14 23:53:24 -0300

    chore: exception handler

---
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/ControllerAdvice.java
@@ -0,0 +1,14 @@
+package com.gabrielaraujo.v99_itau_challenge_jr.controllers;
+
+import com.gabrielaraujo.v99_itau_challenge_jr.core.entities.exceptions.OneOrMoreCriteriaWereNotMetException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(OneOrMoreCriteriaWereNotMetException.class)
+    public ResponseEntity<?> manipularExcecao(OneOrMoreCriteriaWereNotMetException excecao) {
+        return ResponseEntity.status(422).build();
+    }
+}
