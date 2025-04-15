3f8ae3d2603c9f4e740afca9c9dc22b883aac7f4
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-14 23:17:48 -0300

    chore: salvar transacao port implementation

---
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/adapters/use_cases/criar_transacao/SalvarTransacaoConsumerPortAdapter.java
@@ -0,0 +1,28 @@
+package com.gabrielaraujo.v99_itau_challenge_jr.adapters.use_cases.criar_transacao;
+
+import com.gabrielaraujo.v99_itau_challenge_jr.adapters.dao.repositories.TransacaoRepository;
+import com.gabrielaraujo.v99_itau_challenge_jr.adapters.dao.tables.TransacaoTableRow;
+import com.gabrielaraujo.v99_itau_challenge_jr.adapters.dao.tables.mappers.TransacaoMapper;
+import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.criar_transacao.implementations.ports.SalvarTransacaoConsumerPort;
+
+public class SalvarTransacaoConsumerPortAdapter implements SalvarTransacaoConsumerPort {
+    private final TransacaoRepository transacaoRepository;
+
+    public SalvarTransacaoConsumerPortAdapter(TransacaoRepository transacaoRepository) {
+        this.transacaoRepository = transacaoRepository;
+    }
+
+    @Override
+    public SalvarTransacaoPortOutput executar(SalvarTransacaoPortInput input) {
+        var novaTransacao = extrairNoFormatoTableDo(input);
+        novaTransacao = transacaoRepository.salvarNova(novaTransacao);
+
+        return SalvarTransacaoPortOutput.builder()
+                .transacao(TransacaoMapper.toEntityFormat(novaTransacao))
+                .build();
+    }
+
+    private TransacaoTableRow extrairNoFormatoTableDo(SalvarTransacaoPortInput input) {
+        return TransacaoMapper.toTableRowFormat(input.getTransacao());
+    }
+}
