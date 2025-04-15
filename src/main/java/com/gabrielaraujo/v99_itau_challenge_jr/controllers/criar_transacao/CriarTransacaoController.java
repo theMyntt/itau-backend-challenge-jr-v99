d86d15b02cf7c5880bf419fda6cdd8b335fa5280
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-15 20:37:54 -0300

    docs: tag for create and delete transaction

--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/criar_transacao/CriarTransacaoController.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/criar_transacao/CriarTransacaoController.java
@@ -2,6 +2,7 @@ package com.gabrielaraujo.v99_itau_challenge_jr.controllers.criar_transacao;
 
 import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.criar_transacao.CriarTransacaoUseCase;
 import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.criar_transacao.io.inputs.CriarTransacaoUseCaseInput;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Transações", description = "Criação e Delete de Transações")
 public class CriarTransacaoController {
     private final CriarTransacaoUseCase criarTransacaoUseCase;
 
--- src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/deletar_transacoes/DeletarTransacoesController.java
+++ src/main/java/com/gabrielaraujo/v99_itau_challenge_jr/controllers/deletar_transacoes/DeletarTransacoesController.java
@@ -1,12 +1,14 @@
 package com.gabrielaraujo.v99_itau_challenge_jr.controllers.deletar_transacoes;
 
 import com.gabrielaraujo.v99_itau_challenge_jr.core.use_cases.deletar_transacoes.DeletarTransacoesUseCase;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Transações", description = "Criação e Delete de Transações")
 public class DeletarTransacoesController {
     private final DeletarTransacoesUseCase deletarTransacoesUseCase;
 
